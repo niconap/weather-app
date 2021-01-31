fae14072d86588fe54899a14af0021044c07e53a
Nico Nap <nico.nap@outlook.com>
2021-01-31 14:34:54 +0100

    Display information on site

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-// Variable to track if error was shown
-let errorShown = false;
+// Array with all searches
+let searches = [];
 
 // Event listener of the search button and selecting the input
 let searchButton = document.getElementById("searchbutton");
@@ -19,7 +19,6 @@ function getWeatherData(location) {
   }).then(async function (response) {
     let json = await response.json();
     if (!json.message) {
-      console.log(json);
       processData(json);
     } else {
       createError(json.message);
@@ -31,13 +30,14 @@ function getWeatherData(location) {
 function processData(json) {
   let data = {
     location: json.name + ", " + json.sys.country,
-    temp: Math.round(json.main.temp) - 273,
-    feels_like: Math.round(json.main.feels_like) - 273,
+    temp: Math.round(json.main.temp) - 273 + " ℃",
+    feels_like: Math.round(json.main.feels_like) - 273 + " ℃",
     humidity: json.main.humidity + "%",
     wind_speed: Math.round(json.wind.speed) + "MPH",
     description: json.weather[0].description,
   };
-  console.log(data);
+  searches = [data, ...searches];
+  render();
 }
 
 // Display an error below the searchbar
@@ -50,4 +50,39 @@ function createError(message) {
   setTimeout(() => {
     search.removeChild(paragraph);
   }, 2000);
+}
+
+// Render all data from the searches array
+function render() {
+  let container = document.getElementById("history");
+  container.textContent = "";
+  searches.forEach((search) => {
+    let main = document.createElement("div");
+    main.id = "historicsearch";
+    let location = document.createElement("h4");
+    location.textContent = search.location;
+    location.id = "location";
+    main.appendChild(location);
+    let description = document.createElement("description");
+    description.textContent = search.description;
+    description.id = "description";
+    main.appendChild(description);
+    let temp = document.createElement("p");
+    temp.textContent = search.temp;
+    temp.id = "temp";
+    main.appendChild(temp);
+    let feels_like = document.createElement("p");
+    feels_like.textContent = "Feels like: " + search.feels_like;
+    feels_like.id = "feels_like";
+    main.appendChild(feels_like);
+    let humidity = document.createElement("p");
+    humidity.textContent = "Humidity: " + search.humidity;
+    humidity.id = "humidity";
+    main.appendChild(humidity);
+    let wind_speed = document.createElement("p");
+    wind_speed.textContent = "Wind speed: " + search.wind_speed;
+    wind_speed.id = "wind_speed";
+    main.appendChild(wind_speed);
+    container.appendChild(main);
+  });
 }
